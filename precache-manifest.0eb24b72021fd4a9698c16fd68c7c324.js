a006734e5086dfc9defe4ff1c179f1b536a380da
iclasschima <iclasschima@gmail.com>
2019-12-01 18:12:05 +0100

    Updates

---
+++ precache-manifest.0eb24b72021fd4a9698c16fd68c7c324.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "845dd4685c2eb99103e87421bac7efcb",
+    "url": "/iClass2020/index.html"
+  },
+  {
+    "revision": "0ac854afea329de2964e",
+    "url": "/iClass2020/static/css/2.64a99b10.chunk.css"
+  },
+  {
+    "revision": "c12323974d66625af60a",
+    "url": "/iClass2020/static/css/main.db3b3f40.chunk.css"
+  },
+  {
+    "revision": "0ac854afea329de2964e",
+    "url": "/iClass2020/static/js/2.9c7e4a71.chunk.js"
+  },
+  {
+    "revision": "c12323974d66625af60a",
+    "url": "/iClass2020/static/js/main.accab44b.chunk.js"
+  },
+  {
+    "revision": "a660053fd6759a63cba9",
+    "url": "/iClass2020/static/js/runtime-main.13b5ccdb.js"
+  },
+  {
+    "revision": "25a28064a030eb187e8e7f63b8ad56d4",
+    "url": "/iClass2020/static/media/rainbows_and_fireworks.25a28064.jpg"
+  }
+]);
